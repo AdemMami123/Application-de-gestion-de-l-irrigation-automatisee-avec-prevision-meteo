472d3e037f0797c8e2b37792f9baa7c5c644b8d1
Adem Mami <adem.mami@itbs.tn>
2026-01-09 10:53:24 +0100

    fixed issues

--- start-arrosage-service.ps1
+++
@@ -1,7 +0,0 @@
-# Start Arrosage Service  
-Write-Host "========================================" -ForegroundColor Green
-Write-Host " Starting Arrosage Service on port 8082" -ForegroundColor Green
-Write-Host "========================================" -ForegroundColor Green
-
-cd backend\arrosage-service
-java -jar target\arrosage-service-0.0.1-SNAPSHOT.jar
